668fc78a9bd12290aa141e1d17f1539b3ef4f40f
tiesen243 <ttien56906@gmail.com>
2025-04-21 03:52:51 +0700

    feat: add config files

---
+++ eslint.config.js
@@ -0,0 +1,132 @@
+// @ts-nocheck
+
+import jseslint from '@eslint/js'
+import nextPlugin from '@next/eslint-plugin-next'
+import importPlugin from 'eslint-plugin-import'
+import reactPlugin from 'eslint-plugin-react'
+import hooksPlugin from 'eslint-plugin-react-hooks'
+import turboPlugin from 'eslint-plugin-turbo'
+import tseslint from 'typescript-eslint'
+
+/**
+ * All packages that leverage @yuki/env should use this rule
+ */
+const restrictEnvAccess = tseslint.config(
+  { ignores: ['**/env.ts'] },
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    rules: {
+      'no-restricted-properties': [
+        'error',
+        {
+          object: 'process',
+          property: 'env',
+          message:
+            "Use `import { env } from '@/env'` instead to ensure validated types.",
+        },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          name: 'process',
+          importNames: ['env'],
+          message:
+            "Use `import { env } from '@/env'` instead to ensure validated types.",
+        },
+      ],
+    },
+  },
+)
+
+const baseConfig = tseslint.config(
+  { ignores: ['*.config.js'] },
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    plugins: { import: importPlugin },
+    extends: [
+      jseslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+      '@typescript-eslint/no-unnecessary-condition': [
+        'error',
+        { allowConstantLoopConditions: true },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/prefer-promise-reject-errors': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'import/no-anonymous-default-export': 'warn',
+    },
+  },
+  {
+    plugins: { turbo: turboPlugin },
+    rules: {
+      ...turboPlugin.configs.recommended.rules,
+      'turbo/no-undeclared-env-vars': 'warn',
+    },
+  },
+  {
+    linterOptions: { reportUnusedDisableDirectives: true },
+    languageOptions: { parserOptions: { projectService: true } },
+  },
+)
+
+/**
+ * A custom ESLint configuration for libraries that use React.
+ *
+ * @type {Awaited<import('typescript-eslint').Config>} */
+const reactConfig = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': hooksPlugin,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...hooksPlugin.configs.recommended.rules,
+
+      'react/no-unknown-property': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+]
+
+/** @type {Awaited<import('typescript-eslint').Config>} */
+const nextjsConfig = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+]
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: ['.next/**'],
+  },
+  ...baseConfig,
+  ...reactConfig,
+  ...nextjsConfig,
+  ...restrictEnvAccess,
+]
